57306daca1cf77be2c4ea7a19dd36e74b3bbf353
nick <nick>
2022-01-06 01:54:50 +0000

    Don't write game to stdout so kubernetes doesn't log it and own my sd cards.

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sysctl -w kernel.core_pattern=/tmp/cores/core-%e.%p.%h.%t
 cd /app/dnh/dnethackdir
-/app/dnh/dnethackdir/dnethack -D -u `hostname`
+/app/dnh/dnethackdir/dnethack -D -u `hostname` > /dev/null
 sleep 10
 cp /tmp/cores/* /cores
 if [ -e /cores/dnh/dnethackdir/dnethack ]
