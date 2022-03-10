e05fbb9d99eee8e0631be627f029e7c27daf0a1c
nick <nick>
2022-03-10 18:09:41 +0000

    Add ndnh fuzzing.

--- init.sh
+++ init.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 sysctl -w kernel.core_pattern=/tmp/cores/core-%e.%p.%h.%t
-cd /app/dnh/dnethackdir
-/app/dnh/dnethackdir/dnethack -D -u `hostname` > /dev/null
+cd /app/dnh/notdnethackdir
+/app/dnh/notdnethackdir/notdnethack -D -u `hostname` > /dev/null
 sleep 10
 cp /tmp/cores/* /cores
-if [ -e /cores/dnh/dnethackdir/dnethack ]
+if [ -e /cores/dnh/notdnethackdir/notdnethack ]
 then
-	echo "DNH install exists, checking hash"
-	ourHash=$(md5sum /app/dnh/dnethackdir/dnethack | cut -f 1 -d ' ')
-	theirHash=$(md5sum /cores/dnh/dnethackdir/dnethack | cut -f 1 -d ' ')
+	echo "nDNH install exists, checking hash"
+	ourHash=$(md5sum /app/dnh/notdnethackdir/notdnethack | cut -f 1 -d ' ')
+	theirHash=$(md5sum /cores/dnh/notdnethackdir/notdnethack | cut -f 1 -d ' ')
 	echo "$ourHash, $theirHash"
 	if [[ "$ourHash" == "$theirHash" ]]
 	then
-		echo "DNH install in date, exiting"
+		echo "nDNH install in date, exiting"
 	else
-		echo "DNH install out of date, updating"
+		echo "nDNH install out of date, updating"
 		rm -rf /cores/dnh
 		cp -r /app/dnh /cores/dnh
 	fi
 else
-	echo "DNH install nonexistent, updating"
+	echo "nDNH install nonexistent, updating"
 	rm -rf /cores/dnh
 	cp -r /app/dnh /cores/dnh
 fi
